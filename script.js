20dba7cf7fcab5068f77bd499e482bcf43566e2c
justinofimbert <justinofimbert@gmail.com>
2022-02-23 01:55:01 -0300

    created grid line toggle button that maintains its state after changing the number of squares of the grid

--- script.js
+++ script.js
@@ -23,8 +23,15 @@ function createGrid(gridSize, firstGrid = false) {
 
   const clearButton = document.querySelector("#clear-button");
   clearButton.addEventListener("click", () => squares.forEach(square => square.style.backgroundColor = "white"));
+
+  toggleGridButton.addEventListener("click", () => squares.forEach(square => square.classList.toggle("grid-lines")))
+  if (toggleGridButton.classList.contains("active")) {
+    squares.forEach(square => square.classList.toggle("grid-lines"));
+  }
 }
 const bigContainer = document.querySelector("#column");
+const toggleGridButton = document.querySelector("#toggle-grid-lines");
+toggleGridButton.addEventListener("click", () => toggleGridButton.classList.toggle("active"));
 
 createGrid(16, true);
 
@@ -32,4 +39,4 @@ const sizeButtons = Array.from(document.querySelectorAll("#buttons button"));
 sizeButtons.forEach(button => button.addEventListener("click", () => {
   const size = parseInt(button.id);
   createGrid(size);
-}));//createGrid
+}));
